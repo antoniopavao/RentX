2b7d420a0771ba9c3f470f5c53bd3f67be8f794e
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2021-09-07 19:02:53 -0300

    First commit

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+import { categoriesRoutes } from "./routes/categories.routes";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(categoriesRoutes);
+
+const port = 3333;
+
+app.listen(port, () => console.log("Server is running"));
+
+// Tabela para carros, todo carro tera uma categoria, fotos, especificacoes
+// Relacionamento de muitos para muitos
