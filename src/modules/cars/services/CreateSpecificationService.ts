6560bc52cac5840e608563035575f7fc2d5d86c2
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2021-09-14 22:30:18 -0300

    Creating Specification Routes

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,20 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
 class CreateSpecificationService {
-    execute() {
-        console.log("here");
+    constructor(private specificationsRepository: ISpecificationsRepository) {}
+    execute({ name, description }: IRequest): void {
+        const specificationAlreadyExists =
+            this.specificationsRepository.findByName(name);
+        if (specificationAlreadyExists) {
+            throw new Error(`Specification ${name} already exists`);
+        }
+
+        this.specificationsRepository.create({ name, description });
     }
 }
 
