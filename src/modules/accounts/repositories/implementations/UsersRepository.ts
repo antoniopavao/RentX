9efd436b67857785658db002ae6341aea957cfc4
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2021-10-30 16:45:55 -0300

    Removing username column

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -12,14 +12,12 @@ class UsersRepository implements IUsersRepository {
     }
     async create({
         name,
-        username,
         password,
         email,
         driver_license,
     }: ICreateUserDTO): Promise<void> {
         const user = this.repository.create({
             name,
-            username,
             email,
             driver_license,
             password,
