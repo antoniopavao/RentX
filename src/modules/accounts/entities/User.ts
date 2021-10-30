1f2bc0b2a8d974601d6a3c6766f485d02fd6a7be
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2021-10-30 15:33:21 -0300

    Creating user Entity

---
+++ src/modules/accounts/entities/User.ts
@@ -0,0 +1,31 @@
+import { Column, Entity, CreateDateColumn, PrimaryColumn } from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+
+@Entity("users")
+class User {
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    username: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    driver_license: string;
+
+    @Column()
+    isAdmin: boolean;
+
+    @CreateDateColumn()
+    created_at: Date;
+}
+
+export { User };
