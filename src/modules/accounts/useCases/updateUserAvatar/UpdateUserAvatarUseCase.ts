3d58ea5adbb5206687fb457db4ab2e75a0cb60d7
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2021-11-02 22:17:40 -0300

    Creating updateUserAvatar functions

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,2 +1,23 @@
-class UpdateUserAvatarUseCase {}
+import { inject } from "tsyringe";
+
+import { UsersRepository } from "../../repositories/implementations/UsersRepository";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+    user_id: string;
+    avatar_file: string;
+}
+
+class UpdateUserAvatarUseCase {
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository
+    ) {}
+    async execute({ user_id, avatar_file }: IRequest): Promise<void> {
+        const user = await this.usersRepository.findById(user_id);
+        user.avatar = avatar_file;
+
+        await this.usersRepository.create(user);
+    }
+}
 export { UpdateUserAvatarUseCase };
