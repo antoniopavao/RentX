3949e87b8ccfcf1dfa79ebd016eb45564cae70d5
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2021-11-05 13:28:51 -0300

    Creating test

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,8 +1,15 @@
 describe("Create category", () => {
     it("should return 4", () => {
         const soma = 2 + 2;
-        const result = 5;
+        const result = 4;
 
         expect(soma).toBe(result);
+    });
+
+    it("espero que 2 + 2 nao seja 5", () => {
+        const soma = 2 + 2;
+        const result = 5;
+
+        expect(soma).not.toBe(result);
     });
 });
