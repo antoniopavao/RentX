24093c778fa4f2704acc09911898279d4da169c3
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2021-11-09 22:03:00 -0300

    Refactoring project

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,7 +1,7 @@
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
-import "../../../database";
+import "../typeorm";
 import "dotenv/config";
 import "../../container";
 
