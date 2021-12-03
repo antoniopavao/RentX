e8e6a88c83eea0b8f6c81d732bec4f2f2e77f1ea
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2021-12-03 15:22:51 -0300

    Fixing provider error

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,4 +3,7 @@ import { container } from "tsyringe";
 import { IDateProvider } from "./DateProvider/IDateProvider";
 import { DayJsDateProvider } from "./DateProvider/implementations/DayJsDateProvider";
 
-container.registerSingleton<IDateProvider>("DateProvider", DayJsDateProvider);
+container.registerSingleton<IDateProvider>(
+    "DayJsDateProvider",
+    DayJsDateProvider
+);
