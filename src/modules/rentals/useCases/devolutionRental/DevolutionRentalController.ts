dca0533448f1bf86aec8e801330dbeb7881e004f
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2021-12-27 20:33:31 -0300

    Finishing DevolutionRentalController

--- src/modules/rentals/useCases/devolutionRental/DevolutionRentalController.ts
+++ src/modules/rentals/useCases/devolutionRental/DevolutionRentalController.ts
@@ -1,17 +1,23 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
 
-import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
-
 import { DevolutionRentalUseCase } from "./DevolutionRentalUseCase";
 
 class DevolutionRentalController {
     async handle(request: Request, response: Response): Promise<Response> {
+        const { id: user_id } = request.user;
+        const { id } = request.params;
+
         const devolutionRentalUseCase = container.resolve(
             DevolutionRentalUseCase
         );
 
-        // return response.json(updateAvailableRental);
+        const rental = await devolutionRentalUseCase.execute({
+            id,
+            user_id,
+        });
+
+        return response.status(200).json(rental);
     }
 }
 
