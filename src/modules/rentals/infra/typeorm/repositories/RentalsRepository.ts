84d3114db5b8be7fbb717fa6799e7fd502daae47
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2021-12-28 10:01:13 -0300

    findByUser to find all rentals by user

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -52,6 +52,10 @@ class RentalsRepository implements IRentalsRepository {
         const rental = await this.repository.findOne(id);
         return rental;
     }
+    async findByUser(user_id: string): Promise<Rental[]> {
+        const userRentals = await this.repository.find({ user_id });
+        return userRentals;
+    }
 }
 
 export { RentalsRepository };
--- src/modules/rentals/useCases/listRentalByUser/ListRentalByUserUseCase.ts
+++ src/modules/rentals/useCases/listRentalByUser/ListRentalByUserUseCase.ts
@@ -1,2 +1,18 @@
-class ListRentalByUserUseCase {}
+import { inject } from "tsyringe";
+
+import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+
+class ListRentalByUserUseCase {
+    constructor(
+        @inject("RentalsRepository")
+        private rentalsRepository: IRentalsRepository
+    ) {}
+
+    async execute(user_id: string): Promise<Rental[]> {
+        const userRentals = await this.rentalsRepository.findByUser(user_id);
+
+        return userRentals;
+    }
+}
 export { ListRentalByUserUseCase };
