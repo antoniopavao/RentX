f4394519c99d7f9bc2613995d3648930193abb29
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2022-01-05 20:55:17 -0300

    Fix auth sending the refresh instead of the token

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
-import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
 
 import { AppError } from "../../../errors/AppError";
 
@@ -16,6 +16,7 @@ export async function ensureAuthenticated(
     next: NextFunction
 ) {
     const authHeader = request.headers.authorization;
+    const userTokensRepository = new UsersTokensRepository();
 
     if (!authHeader) {
         throw new AppError("Token missing", 401);
@@ -25,12 +26,13 @@ export async function ensureAuthenticated(
     try {
         const { sub: user_id } = verify(
             token,
-            process.env.JWT_SECRET_KEY
+            process.env.REFRESH_TOKEN_SECRET_KEY
         ) as IPayload;
 
-        const usersRepository = new UsersRepository();
-
-        const user = await usersRepository.findById(user_id);
+        const user = await userTokensRepository.findByUserIdAndRefreshToken(
+            user_id,
+            token
+        );
 
         if (!user) {
             throw new AppError(`User does not exists`, 404);
