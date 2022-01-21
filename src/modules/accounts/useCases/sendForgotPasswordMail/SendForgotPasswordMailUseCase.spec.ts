d28bbbd2ad49c3228da5e89570d205d35631c489
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2022-01-20 23:23:36 -0300

    Finishing SendForgotPasswordMailUseCase.spec.ts

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.spec.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.spec.ts
@@ -2,6 +2,7 @@ import { UsersRepositoryInMemory } from "@modules/accounts/repositories/in-memor
 import { UsersTokensRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersTokensRepositoryInMemory";
 import { DayJsDateProvider } from "@shared/container/providers/DateProvider/implementations/DayJsDateProvider";
 import { MailProviderInMemory } from "@shared/container/providers/MailProvider/in-memory/MailProviderInMemory";
+import { AppError } from "@shared/errors/AppError";
 
 import { SendForgotPasswordMailUseCase } from "./SendForgotPasswordMailUseCase";
 
@@ -26,15 +27,38 @@ describe("Send Forgot Mail", () => {
         );
     });
     it("Should be able to send a forgot password mail to user", async () => {
-        const sendMail = spyOn(mailProvider, "sendMail");
+        const sendMail = jest.spyOn(mailProvider, "sendMail");
         await usersRepositoryInMemory.create({
             driver_license: "666146",
             email: "user@example.com",
             name: "Blanche Curry",
             password: "1234",
         });
-        await sendForgotPasswordMailUseCase.execute("user@example.co");
+        await sendForgotPasswordMailUseCase.execute("user@example.com");
 
         expect(sendMail).toHaveBeenCalled();
+    });
+
+    it("Should not be able to send a forgot password mail if user does not exists", async () => {
+        await expect(
+            sendForgotPasswordMailUseCase.execute("idontexist@gmail.com")
+        ).rejects.toEqual(new AppError("User does not exists!"));
+    });
+    it("Should be able to create an users token", async () => {
+        const generateTokenMail = jest.spyOn(
+            usersTokensRepositoryInMemory,
+            "create"
+        );
+
+        await usersRepositoryInMemory.create({
+            driver_license: "91238129",
+            email: "user2@example.com",
+            name: "Weed Nilson",
+            password: "1234",
+        });
+
+        await sendForgotPasswordMailUseCase.execute("user2@example.com");
+
+        expect(generateTokenMail).toBeCalled();
     });
 });
