238576bb4388ce2de994daa2b7caf69b3b673ef3
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2022-01-20 23:34:48 -0300

    Fixing jest config to have coverage report

--- jest.config.ts
+++ jest.config.ts
@@ -16,13 +16,13 @@ export default {
     clearMocks: true,
 
     // Indicates whether the coverage information should be collected while executing the test
-    // collectCoverage: true,
+    collectCoverage: true,
 
     // An array of glob patterns indicating a set of files for which coverage information should be collected
-    // collectCoverageFrom: ["<rootDir>/src/modules/**/useCases/**/*.ts"],
+    collectCoverageFrom: ["<rootDir>/src/modules/**/useCases/**/*.ts"],
 
     // The directory where Jest should output its coverage files
-    // coverageDirectory: "coverage",
+    coverageDirectory: "coverage",
 
     // An array of regexp pattern strings used to skip coverage collection
     // coveragePathIgnorePatterns: [
@@ -30,10 +30,10 @@ export default {
     // ],
 
     // Indicates which provider should be used to instrument code for coverage
-    // coverageProvider: "v8",
+    coverageProvider: "v8",
 
     // A list of reporter names that Jest uses when writing coverage reports
-    // coverageReporters: ["text-summary", "lcov"],
+    coverageReporters: ["text-summary", "lcov"],
 
     // An object that configures minimum threshold enforcement for coverage results
     // coverageThreshold: undefined,
